a98aee74c589198ede28591b159679ab779e2ed3
boo <boo@boo.localhost>
2020-09-05 18:50:51 +0200

    Php mvc composer project.

--- app/Config/AppConfig.php
+++ app/Config/AppConfig.php
@@ -3,9 +3,21 @@ namespace App\Config;
 
 class AppConfig
 {
+    // Mysql
     const MYSQL_HOST = 'localhost';
     const MYSQL_PORT = '3006';
     const MYSQL_DBNAME = 'homestead';
     const MYSQL_USER = 'root';
     const MYSQL_PASS = 'toor';
+
+    // Smtp (default localhost without pass)
+	const SMTP_USER = '';
+	const SMTP_PASS = '';
+	const SMTP_HOST = '127.0.0.1';
+	const SMTP_PORT = 25; // 25, 587, 465
+	const SMTP_FROM_EMAIL = 'no-reply@local.host';
+	const SMTP_FROM_USER = 'Welcome';
+	const SMTP_TLS = false; // true - enabled ssl connection
+	const SMTP_AUTH = false; // true - enabled authentication
+	const SMTP_DEBUG = 0;
 }
--- app/Http/Controller/Homepage.php
+++ app/Http/Controller/Homepage.php
@@ -1,21 +1,32 @@
 <?php
 namespace App\Http\Controller;
 
-use App\Http\View\HomeView;
 use Xo\Db\Mysql\Db;
+use Xo\Mail\Send\SendEmail;
+use App\Http\View\HomeView;
 
 class Homepage extends Controller
 {
-    function Index()
-    {
-        // From Controller
-        $pa = $this->ParseUri();
-		$id = $this->UriParam(2);
+	function Index()
+	{
+		try
+		{
+			// Send email
+			// echo SendEmail::Send('to@local.host', 'Subject', '<html>Html message here</html>');
 
-		// Mysql
-		$row = Db::Query("SELECT * FROM users WHERE id = :id", [':id' => 1])->Fetch();
+			// From Controller
+			$pa = $this->ParseUri();
+			$id = $this->UriParam(2);
 
-        // Get html
-        return HomeView::Html(['id' => $id, 'pa' => $pa, 'row' => $row]);
-    }
+			// Mysql
+			$row = Db::Query("SELECT * FROM users WHERE id = :id", [':id' => 1])->Fetch();
+
+			// Get html
+			return HomeView::Html(['id' => $id, 'pa' => $pa, 'row' => $row]);
+		}
+		catch(Exception $e)
+		{
+			echo $e->getMessage();
+		}
+	}
 }
