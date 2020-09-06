39e82a0ab5ee1399dfaae07ac9fc3fbfdcc69444
boo <boo@boo.localhost>
2020-09-06 11:49:22 +0200

    Php mvc composer project.

--- app/Http/Controller/Homepage.php
+++ app/Http/Controller/Homepage.php
@@ -14,10 +14,12 @@ class Homepage extends Controller
 			// Send email
 			// $ok = SendEmail::Send('to@local.host', 'Subject', '<html>Html message here</html>');
 
-			// Mysql query
-			// $row = Db::Query("SELECT * FROM users WHERE id = :id", [':id' => 1])->Fetch();
-			// $rows = Db::Query("SELECT * FROM users WHERE id > :id", [':id' => 0])->FetchAll();
-			// $lid = Db::Query("INSERT INTO users(email,pass) VALUES(:e,:p)", [':e' => 'email@page.xx', ':p' => md5('pass')])->LastInsertId();
+			// Mysql select
+			// $row = Db::Query("SELECT * FROM user WHERE id = :id", [':id' => 1])->Fetch();
+			// $rows = Db::Query("SELECT * FROM user WHERE id > :id", [':id' => 0])->FetchAll();
+			// Mysql insert
+			// $arr = [':e' => rand(0,9999).'email@page.xx', ':p' => md5('pass'), ':i' => '127.0.0.1'];
+			// $lid = Db::Query("INSERT INTO user(email,password,ip) VALUES(:e,:p,:i)", $arr)->LastInsertId();
 
 			// Methods from controller
 			$pa = $this->ParseUri();
