5b9de2273fe855e9047c2e4c6a51ac84d51e9410
boo <boo@boo.localhost>
2020-09-08 21:45:37 +0200

    Php mvc composer project.

--- app/Http/View/HomeView.php
+++ app/Http/View/HomeView.php
@@ -1,12 +1,30 @@
 <?php
 namespace App\Http\View;
 
+use App\Menu\MenuBox;
+
 class HomeView implements View
 {
 	static function Html(array $arr): string
 	{
 		// print_r($arr);
+		$h = " It Works ... " . $arr['id'];
 
-		return " It Works ... " . $arr['id'];
+		// $h .= self::LeftMenu();
+
+		return $h;
+	}
+
+	static function LeftMenu()
+	{
+		$m = new MenuBox('Homepage', '/', '<i class="fas fa-user"></i>');
+		$m->AddLink('Login', '/login');
+		$m->AddLink('Register', '/register');
+		$h = $m->Show();
+
+		// Better insert style from css
+		$h .= $m->Style();
+
+		return $h;
 	}
 }
