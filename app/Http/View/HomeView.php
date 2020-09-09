72cf6ceb948f0d5ea6f40e1a8789d32c9fbbbc53
boo <boo@boo.localhost>
2020-09-09 20:58:26 +0200

    Php mvc rest api (sample form).

--- app/Http/View/HomeView.php
+++ app/Http/View/HomeView.php
@@ -9,11 +9,36 @@ class HomeView implements View
 	{
 		// print_r($arr);
 
-		$h = " It Works ... " . $arr['id'];
+		$h = "<h3> It Works ... </h3>";
 
-		// Add left menu
+		// Sample form
+		$h .= self::ShowForm();
+
+		// Add menu
 		// $h .= LeftMenu::Html();
 
 		return $h;
+	}
+
+	static function ShowForm()
+	{
+		return '
+		<link href="/style.css" rel="stylesheet"> <script defer src="/main.js"></script>
+		<form method="post">
+			<h2>Awesome</h2>
+			<label>Email addres</label>
+			<input type="text" placeholder="Enter email address">
+			<label>Gender</label>
+			<select>
+				<option>MALE</option>
+				<option>FEMALE</option>
+			</select>
+			<label>Message</label>
+			<textarea placeholder="Enter message"></textarea>
+			<input type="checkbox" name="scales" checked>
+			<input type="radio" name="drone" value="huey" checked>
+			<input type="radio" id="dewey" name="drone" value="dewey">
+			<input type="submit" value="Button">
+		</form>';
 	}
 }
