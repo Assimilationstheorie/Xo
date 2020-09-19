0e2b077ab126adb53724d95cf64cb9067a8dcf6a
boo <boo@boo.localhost>
2020-09-19 15:39:59 +0200

    Update html header

--- app/Http/Component/Html.php
+++ app/Http/Component/Html.php
@@ -10,7 +10,7 @@ return '<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="robots" content="index,follow">
     <title>'.$title.'</title>
 	<meta name="description" content="'.$description.'">
