fbc4d22702e885c31d7ec9ed8089cb19cfedf293
boo <boo@boo.localhost>
2020-10-14 21:02:25 +0200

    Panel menu

--- app/Http/View/Menu/MenuTop.php
+++ app/Http/View/Menu/MenuTop.php
@@ -8,19 +8,21 @@ class MenuTop
 		return '
 			<div class="menu-top">
 				<div class="logo">
-					<img src="/media/img/cross.png">
+					<img src="/media/img/logo.png">
 				</div>
 				<div class="search">
 					<input type="text" placeholder="Search" class="search-input">
 				</div>
 				<div class="notify">
-					<i class="fas fa-bell"></i>
+					<div class="notify-icon"> <i class="fas fa-bell"></i> <span> 1 </span> </div>
+					<div class="notify-icon"> <i class="fas fa-envelope"></i> <span> 23 </span> </div>
+					<div class="notify-icon"> <i class="fas fa-comment"></i> <span> 69 </span> </div>
 				</div>
 				<div class="cogs">
 					<i class="fas fa-cog"></i> <span> Settings </span>
 				</div>
 				<div class="avatar">
-					<img src="/media/img/cross.png">
+					<img src="/media/img/avatar.jpg">
 				</div>
 			</div>
 		';
