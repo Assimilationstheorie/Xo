0a492429024ddc2699f16d41de2d26894d1f8e91
boo <boo@boo.localhost>
2020-10-15 22:19:39 +0200

    Panel form

--- app/Http/View/Panel/ProfilView.php
+++ app/Http/View/Panel/ProfilView.php
@@ -20,8 +20,44 @@ class ProfilView implements View
 		return MenuTop::Html() . '
 		<div class="content">
 			<div class="box">
-				<h1 class="animate__animated animate__fadeIn"> Profil Info </h1>
-				<p>Sample client panel page. </p>
+				<h1 class="animate__animated animate__fadeIn"> Info </h1>
+				<p> Add some info about you. </p>
+
+				<form class="panel-form"  method="post">
+					<h3>Contact informations</h3>
+
+					<label>Email addres</label>
+					<input type="text" placeholder="Enter email address">
+
+					<label>Alias</label>
+					<input type="text" placeholder="Enter alias / username">
+
+					<label>Phone</label>
+					<input type="text" placeholder="Enter phone">
+
+					<label>Gender</label>
+					<select>
+						<option>Male</option>
+						<option>Female</option>
+					</select>
+
+					<input type="submit" value="Button">
+				</form>
+
+				<form class="panel-form"  method="post">
+					<h3>Address</h3>
+
+					<label>Country</label>
+					<input type="text" placeholder="Enter country">
+
+					<label>City</label>
+					<input type="text" placeholder="Enter city">
+
+					<label>Address</label>
+					<input type="password" placeholder="Enter address">
+
+					<input type="submit" value="Button">
+				</form>
 			</div>
 		</div>
 		';
